d795577a4b3737fbb8440f2a71b939984e95d741
Thommzy <tomiwaobeisun@yahoo.com>
2024-07-04 16:17:01 +0100

    Initial Commit

---
+++ Ricky/RickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyApp.swift
+//  Ricky
+//
+//  Created by Timothy Obeisun on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
