54d237d43a5923cc525e2d6c08438de643d0dbba
Thommzy <tomiwaobeisun@yahoo.com>
2024-07-09 17:47:59 +0100

    home view created and logics

--- Ricky/Views/Home/HomeView.swift
+++ Ricky/Views/Home/HomeView.swift
@@ -44,8 +44,8 @@ struct HomeView: View {
 }
 
 
-//struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView()
-//    }
-//}
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ RickyTests/HomeViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModelTests.swift
+//  RickyTests
+//
+//  Created by Timothy Obeisun on 7/8/24.
+//
+
+import Foundation
